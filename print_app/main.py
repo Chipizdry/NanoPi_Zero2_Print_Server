e0c0a1c47c73d94bad749be42b709b20e8b0dee7
Chipizdry <chipizdry@gmail.com>
2025-08-12 12:07:36 +0300

    12/08/2025_Запуск принтсервера

--- print_app/main.py
+++ print_app/main.py
@@ -1,16 +1,24 @@
 
-
 import cups
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Request
+from fastapi.responses import RedirectResponse, HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-CUPS_SERVER = "cups-server"  # имя контейнера
+# Подключаем папки фронта
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+CUPS_SERVER = "cups-server"
 CUPS_PORT = 631
 
+
+
 @app.get("/")
-def root():
-    return {"message": "Print API is running"}
+def redirect_to_static():
+    return RedirectResponse(url="/static/index.html")
 
 @app.post("/print")
 def print_label(content: str = Body(..., embed=True)):
@@ -18,11 +26,11 @@ def print_label(content: str = Body(..., embed=True)):
     printers = conn.getPrinters()
     if not printers:
         return {"error": "No printers found"}
-    
+
     printer_name = list(printers.keys())[0]
     file_path = "/tmp/label.txt"
     with open(file_path, "w") as f:
         f.write(content)
-    
+
     conn.printFile(printer_name, file_path, "Label Job", {})
     return {"status": "Printed", "printer": printer_name}
